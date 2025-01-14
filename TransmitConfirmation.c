76a5f121f0dd4b065e2cf96e2b2845ceaf1ee086
Rohan2039 <rohanskhadd@gmail.com>
2025-01-14 06:22:09 +0000

    Final

--- TransmitConfirmation.c
+++ TransmitConfirmation.c
@@ -7,14 +7,14 @@ bool TXConf_Status = false;
 void userfunction(int* const TxConfReg_ADDR){
 
     /*Please complete the code by following below step:
-    1. Declare an integer pointer and assign it with TxConfReg_ADDR.
-    2. TxConfReg_ADDR is a controller register address which will set by the controller
-       once message trasnitted successfully.
-    3. Wait till ptr is set which means till controller sets the register by using while or any other lop.
-    4. If controller sets the ptr then come out of the loop and make TXConf_Status as true
+    1. Declare an integer pointer and assign it with TxConfReg_ADDR which is given as the argument.
+        INFO :TxConfReg_ADDR is a controller register address which will be set to 1 by the controller
+        once message transmitted successfully.
+    2. Initialise the value at pointer to 0
+    2. Wait till value at ptr is set to 1 by using while or any other loop. // 
+    3. If ptr sets to 1 then come out of the loop and make TXConf_Status as true
 
     Note : Please use the same name TXConf_Status for setting the status to true*/
-
 }
 
 int main(){
